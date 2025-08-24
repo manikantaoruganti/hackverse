afc3415c4c66315b9c26fbbff03244ad8fae7cd0
Manikanta <orugantimanikanta24@gmail.com>
2025-08-24 22:59:45 +0530

    Initial commit

---
+++ frontend/client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then(res => res.text())
+      .then(data => setMessage(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>NovaSphere 🚀</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
